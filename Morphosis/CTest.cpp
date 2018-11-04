02704341aa03ffb00048a47f5ac7f475c6c06ace
mado0421 <sju421@gmail.com>
2018-11-04 21:55:32 +0900

    애니메이션 테스트를 위해서 오브젝트 클래스에 애니메이션 데이터를 더한 테스트용 클래스를 선언

---
+++ Morphosis/CTest.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CTest.h"
+
+
+object::object()
+{
+}
+
+
+object::~object()
+{
+}
---
+++ Morphosis/CTest.h
@@ -0,0 +1,31 @@
+#pragma once
+
+//this class is made for test object class
+class object {
+public:
+	//functions
+
+
+public:
+	//constructor and destructer
+	object();
+	~object();
+
+private:
+	//variables
+
+};
+
+class manageObject {
+public:
+	void Render();
+	void 
+
+
+
+public:
+	manageObject();
+	~manageObject();
+private:
+
+};
