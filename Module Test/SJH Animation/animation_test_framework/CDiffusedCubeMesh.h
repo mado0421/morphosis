841fc4eb5070763a58db77a2402c877ed36ecff6
mado0421 <sju421@gmail.com>
2018-12-28 15:48:23 +0900

    폴더 정리

--- Module Test/SJH Animation/animation_test_framework/CDiffusedCubeMesh.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class CDiffusedCubeMesh : public CMesh
-{
-public:
-	CDiffusedCubeMesh(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList,float fSize);
-	CDiffusedCubeMesh(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList, float fWidth, float fHeight, float fDepth);
-	~CDiffusedCubeMesh();
-};
-
