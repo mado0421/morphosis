2d888895162b9187d40273099dd8b9004b9db574
mado0421 <sju421@gmail.com>
2019-01-26 16:02:49 +0900

    아 왜 안 돼

---
+++ TestClient/TestClient.cpp
@@ -0,0 +1,136 @@
+﻿// TestClient.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <WinSock2.h>
+#include <iostream>
+
+#define SERVERIP	"119.195.130.222"
+#define	SERVERPORT	9000
+#define BUFSIZE		512
+
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+	int received;
+	char *ptr = buf;
+	int left = len;
+
+	while (left > 0)
+	{
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(const char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(const char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+int main()
+{
+
+	int cnt = 0;
+	int retval;
+
+	//	윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	//	socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)err_quit("socket()");
+
+	//	bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)err_quit("bind()");
+
+	//	listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)err_quit("listen()");
+
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	HANDLE hThread;
+	int addrlen;
+
+	//	accept()
+	addrlen = sizeof(clientaddr);
+	client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+	printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+		inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+	if (client_sock == INVALID_SOCKET) {
+		err_display("accept()");
+		return 0;
+	}
+
+	//	데이터 통신에 사용할 변수
+	//	서버와 데이터 통신
+	while (1) {
+		static int test = 0;
+
+		retval = recvn(client_sock, (char*)&test, sizeof(int), 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+		::printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
+
+		test++;
+
+		//	데이터 받기
+		retval = send(client_sock, (char*)&test, sizeof(int), 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+			break;
+		}
+		else if (retval == 0)
+			break;
+	}
+
+	//	close_socket()
+	closesocket(listen_sock);
+
+	//	윈속 종료
+	WSACleanup();
+	::printf("프로그램이 종료됨.\n");
+	return 0;
+
+	std::cout << "Hello World!\n";
+}
+
